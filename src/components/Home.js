140f5d9a5f7d007d4c1889074ccdbe440d1561d0
Livesh kumar <57588397+dev-osiris@users.noreply.github.com>
2023-04-25 11:01:55 +0530

    added title image, changed font

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 const Home = () => {
     return (
-      <div className="side-padding">
+      <div className="side-padding Home-text">
         <h1>To buy exclusive laptops with a wide variety of brands click now!</h1>
 
         <div className="side-by-side" style={{backgroundColor: "#ececd2", textAlign: "left"}}>
-          <div style={{paddingTop: "5em", display: "grid"}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, laborum dolor!
-            Quibusdam quae ipsam voluptates nobis inventore? Totam veniam magni, possimus quo 
-            voluptatum nulla, officiis nesciunt deleniti et incidunt modi.
+          <div style={{paddingTop: "1em", display: "grid"}}>
+            <p>
+              Welcome to shopEase We understand the importance of having a reliable
+              and efficient laptop, whether you're a student, professional, or simply need a device
+              for entertainment purposes. 
+              Our collection features the latest models from top brands, ensuring you have access to
+              the best technology on the market. Our user-friendly interface and secure payment
+              options make it easy for you to find and purchase the laptop that best suits your needs.
+              Start browsing now and upgrade your laptop today!
+            </p>
 
             <a href="/store">
               <button className="store-btn">Shop Now</button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <a className="title" href="/">ShopEase</a>
+            <a className="title" href="/">
+                <img className="title-image" src={require("./images/Home-text.png")} alt="title" />
+            </a>
 
             <div className="links">
                 <Link to="/">
